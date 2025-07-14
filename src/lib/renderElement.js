46d33c8983b1b73dafae8ad0c365097f2fe6627a
Ycow <skypnal12@gmail.com>
2025-07-14 22:49:01 +0900

    feat: renderElement Add

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -3,7 +3,21 @@ import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
 
+const currentNodeMap = new WeakMap();
+
 export function renderElement(vNode, container) {
+  const currentNodeTree = currentNodeMap.get(container);
+  const progressWorkInNodeTree = normalizeVNode(vNode);
+
+  if (!currentNodeTree) {
+    container.appendChild(createElement(progressWorkInNodeTree));
+  } else {
+    updateElement(container, progressWorkInNodeTree, currentNodeTree);
+  }
+
+  currentNodeMap.set(container, progressWorkInNodeTree);
+  setupEventListeners(container);
+
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
