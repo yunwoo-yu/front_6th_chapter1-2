056c5965fc7e291f3afada3e228ad1896c74cc23
Ycow <skypnal12@gmail.com>
2025-07-15 01:29:52 +0900

    refactor: filter 문법 개선

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -2,7 +2,7 @@ export function createVNode(type, props, ...children) {
   const vNode = {
     type: type,
     props,
-    children: children.flat(Infinity).filter((node) => node !== null && node !== undefined && node !== false),
+    children: children.flat(Infinity).filter((node) => node === 0 || node),
   };
 
   return vNode;
