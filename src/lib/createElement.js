cb69fb57b89b61e3ecfcb673d2d434b23be768aa
Ycow <skypnal12@gmail.com>
2025-07-14 21:27:40 +0900

    feat: createElement Add

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,51 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+function updateAttributes($el, props) {
+  Object.entries(props).forEach(([key, value]) => {
+    if (key.startsWith("on") && typeof value === "function") {
+      const eventType = key.slice(2).toLowerCase();
 
-function updateAttributes($el, props) {}
+      addEvent($el, eventType, value);
+    } else if (key === "className") {
+      $el.setAttribute("class", value);
+    } else if (key === "style") {
+      Object.assign($el.style, value);
+    } else if (value === true) {
+      $el[key] = value;
+    } else if (value === false) {
+      $el.removeAttribute(key);
+    } else {
+      $el.setAttribute(key, value);
+    }
+  });
+}
+
+export function createElement(vNode) {
+  if (vNode === undefined || vNode === null || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
+
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+
+    vNode.forEach((child) => {
+      fragment.appendChild(createElement(child));
+    });
+
+    return fragment;
+  }
+
+  const $element = document.createElement(vNode.type);
+
+  if (vNode.props) {
+    updateAttributes($element, vNode.props);
+  }
+
+  $element.append(...vNode.children.map((child) => createElement(child)));
+
+  return $element;
+}
