31e46e4e43f5a962f9180cf07373cbedad4ae787
Ycow <skypnal12@gmail.com>
2025-07-14 22:41:36 +0900

    feat: nomalizeVNode Add

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,22 @@
 export function normalizeVNode(vNode) {
+  if (vNode === null || vNode === undefined || vNode === Boolean(vNode)) {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  if (typeof vNode.type === "function") {
+    return normalizeVNode(vNode.type({ ...vNode.props, children: vNode.children }));
+  }
+
+  if (Array.isArray(vNode.children)) {
+    return {
+      ...vNode,
+      children: vNode.children.map((child) => normalizeVNode(child)).filter((child) => child !== ""),
+    };
+  }
+
   return vNode;
 }
